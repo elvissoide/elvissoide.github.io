6ac76317e77838e236a5678b1c0ee454ed71a348
Elvis Guanoluisa <elviserock@gmail.com>
2023-04-13 23:14:19 -0500

    Responsive Design

---
+++ script.js
@@ -0,0 +1,9 @@
+// Mostrar menu responsivo
+function responsiveMenu(){
+    var x = document.getElementById("nav");
+    if(x.className===""){
+        x.className = "responsive";
+    }else{
+        x.className = "";
+    }
+}
